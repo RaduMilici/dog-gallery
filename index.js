f41011275d983911d7b8b126bdd5b0804de60ee8
paula.dragoman <paula.dragoman@3pillarglobal.com>
2019-02-05 13:50:44 +0200

    Create search after breed

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import fetchData from "./fetch.js";
 import "./dogPicture.js";
 
-async function onLoad() {
-    const imageURLs = await fetchData("https://dog.ceo/api/breeds/image/random/3");
+async function onBreedSearch() {
+    clearGallery();
+
+    const imageURLs = await fetchData(`https://dog.ceo/api/breed/${this.value}/images/random/3`);
 
     imageURLs.forEach(URL => {
         const element = document.createElement('dog-picture');
         element.src = URL;
-        document.body.appendChild(element);
+        document.querySelector("#gallery").appendChild(element);
     });
 }
 
-window.addEventListener("load", onLoad);
+function clearGallery() {
+    const gallery = document.querySelector("#gallery");
+
+    while(gallery.firstChild) {
+        gallery.removeChild(gallery.firstChild);
+    }
+}
+
+document.querySelector("#searchBreed").addEventListener("input", onBreedSearch);
